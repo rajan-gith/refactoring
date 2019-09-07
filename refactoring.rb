85267f08b9ac85798aeed13a01e92b759f8a9599
rajan-gith <rajan@satyug.edu.in>
2019-09-07 08:35:17 +0530

    same changes to frquent pointer methods

--- refactoring.rb
+++ refactoring.rb
@@ -46,19 +46,20 @@ class Customer
 		frequent_renter_points = 0
 		result = "Rental Record for #{@name}\n"
 		@rentals.each do |element|
-			# add frequent renter points
-			frequent_renter_points += element.frequent_renter_points
 			# show figures for this rental
 			result += "\t" + element.movie.title + "\t" +  element.charge().to_s + "\n"
 		end
 		#add footer lines
 		result += "Amount owed is #{total_charge}\n"
-		result += "You earned #{frequent_renter_points} frequent renter points"
+		result += "You earned #{total_frequent_renter_points} frequent renter points"
 		result
 	end
 	private
 	def total_charge
 		@rentals.inject(0) {|sum, element| sum + element.charge}
+	end
+	def total_frequent_renter_points
+		@rentals.inject(0) {|sum, element| sum + element.frequent_renter_points}
 	end
 end
 
